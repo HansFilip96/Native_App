87fef7d1038e5df2b24924f3c3f23b51a0b2d5d7
HansFilip96 <samuelsson96.filip@gmail.com>
2022-08-04 01:35:41 +0200

    started with App

---
+++ components/Button.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Text, View, TouchableOpacity, Image } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { COLORS, SIZES, FONTS, SHADOWS } from "../constants";
+
+export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        width: 40,
+        height: 40,
+        backgroundColor: Colors.white,
+        position: "absolute",
+        borderRadius: SIZES.extraLarge,
+        alignItems: "center",
+        justifyContent: "center",
+        ...SHADOWS.light,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Image
+        source={imgUrl}
+        resizeMode="contain"
+        style={{ width: 24, height: 24 }}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export const RectButton = () => {
+  return (
+    <View>
+      <Text> Filip Samuelsson </Text>
+    </View>
+  );
+};
---
+++ components/HomeHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const HomeHeader = () => {
+  return (
+    <View>
+      <Text> Filip Samuelsson </Text>
+    </View>
+  );
+};
+
+export default HomeHeader;
